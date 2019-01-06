ebbbb11fdd87b645089195225cbcb4917a127446
AlexTicovschi <alexmticovschi@gmail.com>
2019-01-06 12:52:31 +0000

    animating the search box

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,27 +1,31 @@
 import React from 'react';
+import {Animated} from "react-animated-css";
 
 const SearchBox = ({ getNews, searchChange }) => {
     return (
         <div className="pa4-l black-80 tc center">
-            <form className="mw7 center pa2 black-80" onSubmit={(e) => e.preventDefault()}>
-            <fieldset className="cf bn ma0 pa0">
-                <div className="cf">
-                <label className="clip">Search</label>
-                <input 
-                    className="ba b--black-20 f5-l input-reset bn fl black-80 bg-white pa3 lh-solid w-100 w-85-m w-90-l br2-ns br--left-ns" 
-                    placeholder="Search for news" 
-                    type="search" 
-                    onChange={searchChange}
-                    // onKeyUp={getNews} 
-                />
-				<input 
-					className="f6 f5-l button-reset fl pv3 tc bn bg-animate bg-black-70 hover-bg-black white pointer w-100 w-15-m w-10-l br2-ns br--right-ns" 
-					type="submit" 
-					value="Search"
-					onClick={getNews}/>
-                </div>
-            </fieldset>
-            </form>
+            <Animated animationIn="zoomIn" isVisible={true} animationInDelay={170}>
+                <form className="mw7 center pa2 black-80" onSubmit={(e) => e.preventDefault()}>
+                    <fieldset className="cf bn ma0 pa0">
+                        <div className="cf">
+                        <label className="clip">Search</label>
+                        <input 
+                            className="ba b--black-20 f5-l input-reset bn fl black-80 bg-white pa3 lh-solid w-100 w-85-m w-90-l br2-ns br--left-ns" 
+                            placeholder="Search for news" 
+                            type="search" 
+                            onChange={searchChange}
+                            // onKeyUp={getNews}
+                        />
+                        <input 
+                            className="f6 f5-l button-reset fl pv3 tc bn bg-animate bg-black-70 hover-bg-black white pointer w-100 w-15-m w-10-l br2-ns br--right-ns" 
+                            type="submit" 
+                            value="Search"
+                            onClick={getNews}
+                        />
+                        </div>
+                    </fieldset>
+                </form>
+            </Animated>
         </div>
 
     )
